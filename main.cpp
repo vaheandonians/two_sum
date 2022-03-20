e4cf30701b61d582b94b5964acc3a2aae7f3b10b
Vahe Andonians <vahe@andonians.com>
2022-03-20 13:52:35 +0100

    cleanup

--- main.cpp
+++ main.cpp
@@ -1,73 +1,8 @@
 #include <iostream>
 #include <vector>
-#include <iterator>
 #include <unordered_map>
-/*
- * negative numbers not handled: [-3,4,3,90] target: 0
-std::vector<int> twoSum(std::vector<int>& nums, int target) {
-    std::vector<int> solution = {0, 0};
-    for (int i = 0; i < nums.size(); i++) {
-        int a = nums[i];
-        if (a > target)
-            continue;
-        else {
-            for (int j = i + 1; j < nums.size(); j++) {
-                int b = nums[j];
-                if (b > target)
-                    continue;
-                if (a + b == target) {
-                    solution[0] = i;
-                    solution[1] = j;
-                    return solution;
-                }
-            }
-        }
-    }
-    return solution;
-}
-*/
 
-/*
- * slow ... O(n^2)
-std::vector<int> twoSum(std::vector<int>& nums, int target) {
-    std::vector<int> solution = {0, 0};
-    for (int i = 0; i < nums.size(); i++) {
-        int a = nums[i];
-        for (int j = i + 1; j < nums.size(); j++) {
-            int b = nums[j];
-            if (a + b == target) {
-                solution[0] = i;
-                solution[1] = j;
-                return solution;
-            }
-        }
-    }
-    return solution;
-}
-*/
-
-/*
-std::vector<int> twoSum(std::vector<int>& nums, int target) {
-    std::vector<int> solution = {0, 0};
-    std::unordered_map<int, int> m;
-    for (int i = 0; i < nums.size(); i++) {
-        int e = nums[i];
-        int targetMinusElement = target - e;
-        try {
-            const int j = m.at(targetMinusElement);
-            solution[1] = i;
-            solution[0] = j;
-            return solution;
-        }
-        catch (const std::out_of_range&) {
-            m.insert({e, i});
-        }
-    }
-    return solution;
-}
-*/
-
-std::vector<int> twoSum(std::vector<int>& nums, int target) {
+std::vector<int> two_sum(std::vector<int>& nums, int target) {
     std::unordered_map<int, int> complement;
     int currentIndex = 0;
     for(int& num : nums) {
@@ -83,18 +18,8 @@ std::vector<int> twoSum(std::vector<int>& nums, int target) {
 }
 
 int main() {
-    // std::cout << "Hello, World!" << std::endl;
     std::vector<int> input = {-3,4,3,90};
-    /*
-    std::vector<int>::iterator ptr;
-    std::cout << "The vector elements are : ";
-    for (ptr = v.begin(); ptr < v.end(); ptr++)
-        std::cout << *ptr << " ";
-    std::cout << std::endl << "Size was: " << v.size() << std::endl;
-
-    std::cout << v[3];
-    */
-    std::vector<int> solution = twoSum(input, 0);
+    std::vector<int> solution = two_sum(input, 0);
     std::cout << solution[0] << " " << solution[1];
     return 0;
 }
